3d6afc2e190fbe62bcb46c78257fb49680fc8617
Jiang Wu <john.x.wu@ericsson.com>
2022-03-09 10:06:21 +0200

    Move zero

---
+++ src/main/java/com/wj/daily/array/MyArray.java
@@ -0,0 +1,36 @@
+package com.wj.daily.array;
+
+public class MyArray {
+    /**
+     * @link: https://leetcode-cn.com/problems/move-zeroes/
+     *
+     * Given an integer array nums, move all 0's to the end of it while maintaining
+     * the relative order of the non-zero elements.
+     *
+     * Note that you must do this in-place without making a copy of the array.
+     *
+     * Example 1:
+     *
+     * Input: nums = [0,1,0,3,12]
+     * Output: [1,3,12,0,0]
+     * Example 2:
+     *
+     * Input: nums = [0]
+     * Output: [0]
+     *
+     * @date   2022/03/09
+     *
+     * @param nums
+     */
+    public void moveZero(int[] nums) {
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                if (i != j) {
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/wj/daily/array/MyArrayTest.java
@@ -0,0 +1,16 @@
+package com.wj.daily.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MyArrayTest {
+
+    public MyArray myArray = new MyArray();
+
+    @Test
+    void testMoveZero() {
+        int[] nums = {0, 1, 0, 3, 12};
+        myArray.moveZero(nums);
+        Assertions.assertArrayEquals(new int[]{1, 3, 12, 0, 0}, nums);
+    }
+}
