5c0eee8af48afe8be6bede1657c1a200b88f44df
wallisnow <john.x.wu@ericsson.com>
2022-03-09 10:25:52 +0200

    change origin link

--- src/main/java/com/wj/daily/array/MyArray.java
+++ src/main/java/com/wj/daily/array/MyArray.java
@@ -10,11 +10,10 @@ public class MyArray {
      * Note that you must do this in-place without making a copy of the array.
      *
      * Example 1:
-     *
      * Input: nums = [0,1,0,3,12]
      * Output: [1,3,12,0,0]
-     * Example 2:
      *
+     * Example 2:
      * Input: nums = [0]
      * Output: [0]
      *
